32dfb704ca35646cd780e629fefd8fb5648404ac
Alex Bondar <abondar1992@gmail.com>
2023-12-01 17:35:34 +0100

    reader: switched to camel rest, added applications yaml, added KafkaProperties and logging

--- Writer/src/main/java/org/abondar/experimental/document/sender/writer/route/UploadRoute.java
+++ Writer/src/main/java/org/abondar/experimental/document/sender/writer/route/UploadRoute.java
@@ -3,69 +3,54 @@ package org.abondar.experimental.document.sender.writer.route;
 import org.abondar.experimental.document.sender.writer.avro.Document;
 import org.abondar.experimental.document.sender.writer.model.DocumentData;
 import org.abondar.experimental.document.sender.writer.parser.DocumentParser;
+import org.abondar.experimental.document.sender.writer.properties.KafkaProperties;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Component
 public class UploadRoute extends RouteBuilder {
 
-    //TODO ADD CAMEL REST
-
-//    @POST
-//    @Path("/upload")
-//    @Consumes(MediaType.MULTIPART_FORM_DATA)
-//    @Produces(MediaType.APPLICATION_JSON)
-//    Response uploadFile(MultipartBody body);
-    private DocumentParser documentParser;
+    private final DocumentParser documentParser;
 
     private DocumentData res;
 
+    private final KafkaProperties kafkaProperties;
+
     @Autowired
-    public UploadRoute(DocumentParser documentParser) {
+    public UploadRoute(DocumentParser documentParser, KafkaProperties kafkaProperties) {
         this.documentParser = documentParser;
+        this.kafkaProperties = kafkaProperties;
     }
 
     @Override
     public void configure() throws Exception {
 
-//        from("cxfrs:bean:jaxRsServer?bindingStyle=SimpleConsumer&synchronous=true")
-//                .routeId("restServiceRoute")
-//                .toD("direct:${headers.operationName}", false);
-//
-//
-//        from("direct:uploadFile").routeId("upload")
-//                .log(LoggingLevel.DEBUG, "#{headers}")
-//                .transform()
-//                .body((bdy, hdrs) -> {
-//                    MultipartBody mBody = (MultipartBody) bdy;
-//                    try {
-//                        var dataHandler = mBody.getAllAttachments().get(0).getDataHandler();
-//                        res = documentParser.parseDocument(dataHandler.getInputStream());
-//
-//                        return Response.ok(ResponseMessageUtil.FILE_UPLOADED).build();
-//                    } catch (IOException | SAXException | TikaException ex) {
-//                        return Response.status(Response.Status.BAD_REQUEST)
-//                                .entity(ResponseMessageUtil.FILE_INCORRECT).build();
-//                    }
-//                })
-//                .wireTap("direct:sendToKafka");
-
+        rest()
+                .post()
+                .path("/upload")
+                .routeId("upload")
+                .consumes("multipart/form-data")
+                .produces("application/json")
+                .apiDocs(true)
+                .to("direct:sendToKafka");
 
         from("direct:sendToKafka").routeId("kafkaSend")
                 .log(LoggingLevel.DEBUG, "#{headers}")
-                .transform()
-                .body((bdy, hdrs) -> {
-
-                   var doc = Document.newBuilder()
+                .process(exchange -> {
+                    var req = exchange.getIn().getBody(MultipartHttpServletRequest.class);
+                    res = documentParser.parseDocument(req.getFile("file").getInputStream());
+                    var doc = Document.newBuilder()
                             .setContent(res.getContent())
                             .setMediaType(res.getMediaType())
                             .setMetadata(res.getMetadata());
-                    return doc.build();
+
+                    exchange.getMessage().setBody(doc.build());
                 })
-                .to("kafka:documentSender?brokers=localhost:9092");
+                .to("kafka:"+kafkaProperties.getTopicName()+"?brokers="+kafkaProperties.getBroker());
     }
 
 }
